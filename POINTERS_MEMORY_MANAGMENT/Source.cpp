694650a327601a5e097c04a3e59e8465305a6d6a
chiranjeevi1112 <chiranjeevia971112@gmail.com>
2026-04-14 12:44:16 +0530

    some pointer things

--- POINTERS_MEMORY_MANAGMENT/Source.cpp
+++ POINTERS_MEMORY_MANAGMENT/Source.cpp
@@ -34,9 +34,51 @@ int main()
 	std::cout << "EConomy1::" << economy1 << std::endl;
 	std::cout << "economy2::" << economy2 << std::endl;
 
-	int *PT = (int*)malloc(sizeof(int));
+	int *PA = (int*)malloc(5*sizeof(int));
 
-		
+	int *bigspace = (int*)malloc(20 * sizeof(int));
+	//derefereccing the first element in the array so that we can intialize the value in it 
+	*bigspace = 10;
+	*(bigspace + 1) = 20;
+	*(bigspace + 2) = 30;
+
+	long* bigints = (long*)malloc(100*sizeof(long));
+
+	for (int i = 0; i < 100; i++, bigints++)
+	{
+		*bigints = 0;
+	}
+
+	//this because we are poinitng the values from the address so that the pointer changes the value from pointning the first element ot the some other element so we
+	// subtracting with the size so that it will point only the first elememt in the array 
+	bigints -= 100;
+
+	int space1[20];
+	int* space2 = space1;
+
+	*space1 = 10;
+
+
+	std::cout << "value of the space1" << *space1 << std::endl;
+	
+
+	space2[1] = 20;
+	std::cout << "value of the space2" << *(space1+1) << std::endl;
+
+	//This double pointer for accessing the elements in the multidimentional array 
+	long table[10][20];
+	long (*ptr )[20] = table;
+	long** ptr_1 = new long* [10];
+
+	long* racing = (long*)malloc(40 * sizeof(long));
+
+
+
+
+
+
+
+	 
 
 	return 0;
 }
